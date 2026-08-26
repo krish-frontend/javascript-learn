eaa4353fb16703dd1bfb910c212039d072eec1ca
Krissh <krrishkumar610@gmail.com>
2026-08-27 03:09:37 +0530

    code practice start pattern loop part1

--- jspractice/starPattern.js
+++ jspractice/starPattern.js
@@ -16,7 +16,7 @@
 // };
 
 
-// //q.2 print star pattern as below as same: -
+// //q.2 print reverse star pattern as below as same: -
 // // *****
 // // ****
 // // ***
@@ -32,3 +32,19 @@
 //     };
 //     console.log(star);
 // }; 
+
+// q.3 Same number pattern as below as same:-
+// 1
+// 22
+// 333
+// 4444
+// 55555
+
+// --------code-------
+
+for(let i=1; i<=5;i++){
+    let star="";
+    for(let j=1;j<=i;j++){
+        console.log(i)
+    }
+}
