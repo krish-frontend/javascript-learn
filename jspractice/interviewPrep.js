12bfcec7ff89778b36108bfed81738fd281803ee
Krissh <krrishkumar610@gmail.com>
2026-09-09 18:16:32 +0530

    code practice loop part10

--- jspractice/interviewPrep.js
+++ jspractice/interviewPrep.js
@@ -37,37 +37,53 @@
 
 // q.3 Find largest and smallest together
 
-case 1
-let numbers = [45, 12, 89, 23, 7, 56];
-let arr = numbers[0];
-let arr2 = numbers[0];
+// case 1
+// let numbers = [45, 12, 89, 23, 7, 56];
+// let arr = numbers[0];
+// let arr2 = numbers[0];
 
-for(let i=0; i<numbers.length; i++){
-   for( let j=i+1; j<numbers.length; j++){
-    if(numbers[j]>arr){
-        arr = numbers[j];
-    }else if(numbers[i]<arr2){
-        arr2 = numbers[i];
+// for(let i=0; i<numbers.length; i++){
+//    for( let j=i+1; j<numbers.length; j++){
+//     if(numbers[j]>arr){
+//         arr = numbers[j];
+//     }else if(numbers[i]<arr2){
+//         arr2 = numbers[i];
+//     };
+//    };
+// };
+
+// console.log(`This is largest number in array = ${arr}`);
+// console.log(`This is smallest number in array = ${arr2}`);
+
+// case 2
+// let largest = numbers[0];
+// let smallest = numbers[0];
+
+// for (let i = 1; i < numbers.length; i++) {
+//     if (numbers[i] > largest) {
+//         largest = numbers[i];
+//     }
+
+//     if (numbers[i] < smallest) {
+//         smallest = numbers[i];
+//     }
+// }
+
+// console.log(`This is largest number in array = ${largest}`);
+// console.log(`This is smallest number in array = ${smallest}`);
+
+
+// q.4 Remove duplicate characters
+
+let str = "programming";
+let str2 = "";
+
+for(let i=0; i<str.length; i++){
+    for(let j=i+1; j<str.length; j++){
+        if(str[i]===str[j]){
+            str2+=str[j];
+            continue;
+        };
     };
-   };
+    console.log(str2);
 };
-
-console.log(`This is largest number in array = ${arr}`);
-console.log(`This is smallest number in array = ${arr2}`);
-
-case 2
-let largest = numbers[0];
-let smallest = numbers[0];
-
-for (let i = 1; i < numbers.length; i++) {
-    if (numbers[i] > largest) {
-        largest = numbers[i];
-    }
-
-    if (numbers[i] < smallest) {
-        smallest = numbers[i];
-    }
-}
-
-console.log(`This is largest number in array = ${largest}`);
-console.log(`This is smallest number in array = ${smallest}`);
