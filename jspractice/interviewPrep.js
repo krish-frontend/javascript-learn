38295f462830dca0e1eb005f0b33c06e39601114
Krissh <krrishkumar610@gmail.com>
2026-09-14 03:16:48 +0530

    code practice loop part11

--- jspractice/interviewPrep.js
+++ jspractice/interviewPrep.js
@@ -180,21 +180,37 @@ let students = [
     {name: "Neha", marks: 32}
 ];
 
-let max =0;
+let max =[];
 let min =0;
 let avg =0;
+let findA40=[];
+let fail=[];
+let topper=[];
 
 for(let i=0; i<students.length; i++){
         //ans.1
         for(let j in students[i]){
                 console.log(`${j}:- ${students[i][j]}`);        //j also access key in object -&&- student[i] = 0 idx, student[i][j] = 0 idx*value
+                
                 if(students[i].marks>max){
                         max=students[i].marks;
                 }else if(students[i].marks<max){
                         min=students[i].marks;
                 };
-                avg+=students[i].marks/j.length
+                
+                if(students[i].marks>=40){
+                        findA40.push(` ${j}:- ${students[i][j]}`)
+                }else if(students[i].marks<40){
+                        fail.push(` ${j}:- ${students[i][j]}`)
+                };
+
+                avg+=students[i].marks/students.length;
         };             
 };
 
-console.log(max, min, avg);
+console.log(max);
+console.log(min);
+console.log(`The average of students:- ${avg}`);
+console.log(`students  score above 40:- {${findA40}}`);
+console.log(`students  score below 40:- {${fail}}`);
+console.log(topper+max)
