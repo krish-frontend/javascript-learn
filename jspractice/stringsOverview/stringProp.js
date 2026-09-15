67d115ef241cdb890c1973387058372456b6a423
Krissh <krrishkumar610@gmail.com>
2026-09-16 01:58:39 +0530

    code practice strings prop n method part 1

--- jspractice/stringsOverview/stringProp.js
+++ jspractice/stringsOverview/stringProp.js
@@ -6,6 +6,27 @@
 // console.log("5"/2);
 
 // Q1 What is the output?
-let str = "JavaScript";
+// let str = "JavaScript";
 
-console.log(str.length);
+// console.log(str.length);
+
+
+// // q.2 find the reason why not changed ?
+// let str2 = "hello";
+
+// console.log(str2.toUpperCase());
+// console.log(str2);
+
+// because we can't modify original string without store in new var
+
+
+
+// -----------intermediate question------
+
+// Q3 — Reverse a String
+
+let str = "Krish";
+
+let rev = str.split("").reverse().join("");
+
+console.log(rev);
