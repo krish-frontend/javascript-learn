80a2801dea76c76b0ee9a9e2cbab31114e619076
Krissh <krrishkumar610@gmail.com>
2026-09-16 02:35:35 +0530

    code practice strings prop n method part 1

--- jspractice/stringsOverview/stringProp.js
+++ jspractice/stringsOverview/stringProp.js
@@ -23,10 +23,30 @@
 
 // -----------intermediate question------
 
-// Q3 — Reverse a String
+// // Q3 — Reverse a String
 
-let str = "Krish";
+// let str = "Krish";
 
-let rev = str.split("").reverse().join("");
+// let rev = str.split("").reverse().join("");
 
-console.log(rev);
+// console.log(rev);
+
+// // Q4 — Count Characters
+
+// let str = "JavaScript";
+
+// let len = str.length;
+
+// console.log(len);
+
+
+// Q5 — Count Vowels
+let str = "javascript";
+let str2 = str.split("");
+
+let count = 0;
+if(str2[]==="a"||str2==="e"||str2==="i"||str2==="o"||str2==="u"){
+    count+=1;
+};
+
+console.log(count);
